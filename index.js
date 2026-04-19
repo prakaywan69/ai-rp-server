e1f3cfcb68c33337480532fae9d93efd81de984d
prakaywan69 <prakaywan69@gmail.com>
2026-04-19 23:06:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import fetch from "node-fetch";
-
+import cors from "cors";
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post("/chat", async (req, res) => {
