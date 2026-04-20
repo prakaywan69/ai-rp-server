257ccc2f5a2b4e0b4c54b90fdb068f3a287b1b6b
prakaywan69 <prakaywan69@gmail.com>
2026-04-20 12:34:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
 import express from "express";
-import fetch from "node-fetch";
-import cors from "cors";
+
 const app = express();
-app.use(cors());
 app.use(express.json());
 
-app.post("/chat", async (req, res) => {
-  const memory = req.body.memory;
+app.post("/chat", (req, res) => {
+  const messages = req.body.messages;
 
-  try {
-    const response = await fetch("https://api.openai.com/v1/responses", {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-  model: "gpt-4.1-mini",
-  input: memory
-})
-    });
-
-    const data = await response.json();
-
-  const reply = data.output_text || "ไม่มีคำตอบ";
-
-    res.json({ reply });
-
-  } catch (err) {
-    res.json({ reply: "error" });
-  }
+  res.json({
+   reply: 'ไซรัสจ้องมองคุณนิ่งๆ ก่อนจะเอ่ยเสียงต่ำ... "เจ้ากำลังพูดถึงอะไร"'
+  });
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
